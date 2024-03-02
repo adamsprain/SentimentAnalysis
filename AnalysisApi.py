74d62fbb53f62994a4222738f333286ed3a3a114
Adam Sprain <ajsprain@wisc.edu>
2024-03-02 12:57:27 -0600

    Basic Prediction API Implemented

--- AnalysisApi.py
+++ AnalysisApi.py
@@ -1,6 +1,31 @@
 from fastapi import FastAPI
+import tensorflow as tf
+import numpy as np
 app = FastAPI()
+model = tf.keras.models.load_model("./sentiment_model")
 
-@app.get("/my-first-api")
-def hello(name: str):
-  return {'Hello ' + name + '!'} 
+@app.get("/")
+def welcome():
+  return {"Welcome": "API for sentiment analysis"}
+
+
+
+@app.get("/prediction")
+def prediction(text: str):
+  prediction = model.predict([text])
+  num_sentiment = np.argmax(prediction[0])
+  match num_sentiment:
+    case 0:
+      return {"prediction": "sadness"}
+    case 1:
+      return {"prediction": "joy"}
+    case 2:
+      return {"prediction": "love"}
+    case 3:
+      return {"prediction": "anger"}
+    case 4:
+      return {"prediction": "fear"}
+    case 5:
+      return {"prediction": "surprise"}
+  return {"Error" : "An error has occured"}
+  
